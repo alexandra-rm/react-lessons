e9009794dcb46d807efe8916b0918354057b6805
alexkhait <alexkhait@yandex-team.ru>
2020-04-06 16:26:08 +0300

    lesson-1: тестовый коммит

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import { sum } from "./sum";
+import { sum } from './sum';
 
-document.body.textContent = "Заиспользуем функцию сложения с урока!";
+document.body.textContent = 'Заиспользуем функцию сложения с урока!';
 
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement('div');
   const a = 2;
   const b = 3;
 
