202849e813b9d3e85377cece79b60d1ea5317513
alexkhait <alexkhait@yandex-team.ru>
2020-04-06 12:45:42 +0300

    lesson-1: правки по ревью

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,14 @@ import { sum } from "./sum";
 
 document.body.textContent = "Заиспользуем функцию сложения с урока!";
 
-
 function component() {
-    const element = document.createElement("div");
-const a = 2;
-const b = 3;
+  const element = document.createElement("div");
+  const a = 2;
+  const b = 3;
 
-    element.innerHTML = `${a} + ${b} = ` + String(sum(2, 3));
+  element.innerHTML = `${a} + ${b} = ` + String(sum(2, 3));
 
-    return element;
+  return element;
 }
 
 document.body.appendChild(component());
